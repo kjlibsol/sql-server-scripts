43601cf8400d360c7114782ef5711d7a1b0c65c1
Klaus Jensen <46519782+kjlibsol@users.noreply.github.com>
2020-03-23 12:07:56 +0200

    Update CreateDbUser.sql

--- CreateDbUser.sql
+++ CreateDbUser.sql
@@ -12,4 +12,4 @@ CREATE USER mynewlogin FROM LOGIN mynewlogin;
 exec sp_addrolemember 'db_owner', 'mynewlogin'
 
 --3a MAKE USER READONLY (datareader)
-exec sp_addrolemember 'db_owner', 'mynewlogin'
+exec sp_addrolemember 'db_datareader', 'mynewlogin'
