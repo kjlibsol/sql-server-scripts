45245f39492baa19440e6c12acb03b98d29e52b9
Klaus Jensen <46519782+kjlibsol@users.noreply.github.com>
2020-03-23 12:06:08 +0200

    Update CreateDbUser.sql

--- CreateDbUser.sql
+++ CreateDbUser.sql
@@ -8,5 +8,8 @@ CREATE LOGIN mynewlogin WITH PASSWORD = 'THISISMYPASSWORD';
 --2 CONNECT TO CLIENT DATABASE
 CREATE USER mynewlogin FROM LOGIN mynewlogin; 
 
+--3a MAKE USER DBO
+exec sp_addrolemember 'db_owner', 'mynewlogin'
 
+--3a MAKE USER READONLY (datareader)
 exec sp_addrolemember 'db_owner', 'mynewlogin'
